7f9bd0738834c6bbb9f588c7afd5e47e4c4000a9
Nemanja Pantoš <npantos@gmail.com>
2018-01-25 19:13:10 +0100

    Zadatak 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/movies',
+        pathMatch: 'full'
+    },
+    {
+        path: 'movies',
+        component: MoviesComponent,
+        children: [
+            {
+                path: ':id',
+                component: MoviesComponent
+            }
+        ]
+    },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MoviesComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      RouterModule.forRoot(
+          appRoutes
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
